d0a975513dd61fd55b32c646af5fe2bfbd03c16f
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-07-18 10:43:15 -0300

    add app test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Container, FloraProvider, globalCss, Flex, Box } from '@grupoboticario/flora-react'
+import { Container, FloraProvider, globalCss, Flex, Box, Button } from '@grupoboticario/flora-react'
 import { Logo } from './components/Logo'
 import { Home } from './components/Home'
+import React, { useState, useEffect } from 'react'
 
 const globalStyle = globalCss({
   'html, body, body > div:first-child, div#__next, div#__next > div': {
@@ -9,9 +10,18 @@ const globalStyle = globalCss({
 })
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `Você clicou ${count} vezes`
+  })
   return (
     <div>
-      <Home />
+      <Box>
+        <p>Clicar {count} times</p>
+        <Button onClick={() => setCount(count + 1)}>Click me</Button>
+        <Home />
+      </Box>
     </div>
   )
 }
